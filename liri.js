034e68542ec364bc85bee2fe7d1f3a62f9902adb
Youngju Lee <lucylee839@hotmail.com>
2019-04-28 11:00:15 -0700

    finished the liribot function

--- liri.js
+++ liri.js
@@ -1,107 +1,162 @@
 require("dotenv").config();
 
+var inquirer = require("inquirer");
 var axios = require("axios");
 var Spotify = require('node-spotify-api');
 var keys = require("./keys.js");
 var moment = require("moment");
 var spotify = new Spotify(keys.spotify);
+var fs = require("fs");
 
-// movie this
-function getMovie(movieName) {
-    if (movieName == undefined) {
-        movieName = "Mr.Nobody";
-    }
-    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-    axios.get(queryUrl).then(
-        function (response) {
-            var movieData = response.data;
+var userCommand = process.argv[2];
+var dataInfo = process.argv.slice(3).join(" ");
 
-            console.log("Title: " + movieData.Title);
-            console.log("Year: " + movieData.Year);
-            console.log("Rated: " + movieData.Rated);
-            console.log("IMDB Rating: " + movieData.imdbRating);
-            console.log("Country: " + movieData.Country);
-            console.log("Language: " + movieData.Language);
-            console.log("Plot: " + movieData.Plot);
-            console.log("Actors: " + movieData.Actors);
-            console.log("#------------------------------------#");
+var liribot = {
+    prompt: function () {
+        inquirer
+            .prompt([
+                {
+                    type: "list",
+                    name: "command",
+                    message: "How can I help you?",
+                    choices: ["movie-this", "spotify-this-song", "concert-this", "do-what-it-says", "quit"]
+                }, {
+                    type: "input",
+                    name: "name",
+                    message: "Who are you???"
+                }
+            ]);
+    },
+    getMovie: function (movieName) {
+        if (movieName == undefined) {
+            movieName = "Mr.Nobody";
         }
-    );
-}
-
-// this spotify
-function getArtist(artist) {
-    return artist.name;
-};
-
-function getSpotify(songName) {
-    if (songName === undefined) {
-        songName = "The Sign";
-    }
-    spotify.search({ type: 'track', query: songName }, function (err, data) {
-        if (err) {
-            return console.log('Error occurred: ' + err);
-        }
-        var songs = data.tracks.items;
-        console.log(data);
-        for (var i in songs) {
-            console.log(i);
-            console.log("artist(s): " + songs[i].artists.map(getArtist));
-            console.log("song name: " + songs[i].name);
-            console.log("preview song: " + songs[i].preview_url);
-            console.log("album: " + songs[i].album.name);
-            console.log("#------------------------------------#");
-        }
-    });
-}
-
-function getConcert(artistName) {
-    if (artistName == undefined) {
-        console.log("please enter artist name!");
-    }
-    var queryUrl = "https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=codingbootcamp";
-    axios.get(queryUrl).then(
-        function (response) {
-            var concertData = response.data;
-            if (!concertData.length) {
-                console.log("No results found for " + artistName);
-                return;
+        var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+        axios.get(queryUrl).then(
+            function (response) {
+                var movieData = response.data;
+                var data = {
+                    "Title ": movieData.Title,
+                    "Year ": movieData.Year,
+                    "Rated ": movieData.Rated,
+                    "IMDB Rating ": movieData.imdbRating,
+                    "Rotten Tomatoes Rating ": movieData.Ratings[1].Value,
+                    "Country ": movieData.Country,
+                    "Language ": movieData.Language,
+                    "Plot ": movieData.Plot,
+                    "Actors ": movieData.Actors
+                };
+                console.log(data);
+                liribot.log(data);
             }
-            console.log("Upcoming concerts for " + artistName + ":");
-            for (var i in concertData) {
-                var concert = concertData[i];
-                console.log("Venue Name : " + concert.venue.name);
-                console.log("Venue Location : " +
-                    concert.venue.city +
-                    "," +
-                    (concert.venue.region || concert.venue.country)
-                );
-                console.log("Time : " + moment(concert.datetime).format("MM/DD/YYYY"));
-                console.log("#------------------------------------#");
-            }
-        }
-    );
+        );
+    },
 
-}
-function run(command, data) {
-    switch (command) {
-        case ("movie-this"):
-            getMovie(data);
-            break;
-        case ("spotify-this-song"):
-            getSpotify(data);
-            break;
-        case ("concert-this"):
-            getConcert(data);
-            break;
-        case "do-what-it-says":
-            doWhatItSays();
-            break;
-        default:
-            console.log("Sorry! I didn't get that");
+    getArtist: function (artist) {
+        return artist.name;
+    },
+
+    getSpotify: function (songName) {
+        if (songName === undefined) {
+            songName = "The Sign";
+        }
+        spotify.search({ type: 'track', query: songName }, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+            var songs = data.tracks.items;
+            var data = [];
+
+            for (var i in songs) {
+                data.push({
+                    "artist(s) ": songs[i].artists.map(liribot.getArtist),
+                    "song name ": songs[i].name,
+                    "preview song ": songs[i].preview_url,
+                    "album ": songs[i].album.name,
+                });
+            }
+            console.log(data);
+            liribot.log(data);
+        });
+    },
+
+    getConcert: function (artistName) {
+        if (artistName == undefined) {
+            console.log("please enter artist name!");
+        }
+        var queryUrl = "https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=codingbootcamp";
+        axios.get(queryUrl).then(
+            function (response) {
+                var concertData = response.data;
+                if (!concertData.length) {
+                    console.log("No results found for " + artistName);
+                    return;
+                }
+
+                var data = [];
+                data.push("Upcoming concerts for " + artistName + ":");
+                for (var i in concertData) {
+                    var concert = concertData[i];
+                    data.push(
+                        concert.venue.city +
+                        "," +
+                        (concert.venue.region || concert.venue.country) +
+                        " at " +
+                        concert.venue.name +
+                        " " +
+                        moment(concert.datetime).format("MM/DD/YYYY")
+                    );
+                }
+                console.log(data);
+                liribot.log(data);
+            }
+        );
+    },
+
+    doWhatItSays: function () {
+        fs.readFile("random.txt", "utf8", function (error, data) {
+            if (error) {
+                return console.log(error);
+            }
+            var dataArr = data.split(",");
+            if (dataArr.length == 2) {
+                liribot.run(dataArr[0], dataArr[1]);
+            } else if (dataArr.length == 1) {
+                liribot.run(dataArr[0]);
+            }
+        });
+    },
+
+    log: function (data) {
+        fs.appendFile("log.txt", JSON.stringify(data) + "\n", function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("Content Added!");
+            }
+        });
+    },
+
+    run: function (command, info) {
+        switch (command) {
+            case ("movie-this"):
+                liribot.getMovie(info);
+                break;
+            case ("spotify-this-song"):
+                liribot.getSpotify(info);
+                break;
+            case ("concert-this"):
+                liribot.getConcert(info);
+                break;
+            case ("do-what-it-says"):
+                liribot.doWhatItSays();
+                break;
+            default:
+                console.log("Sorry! I didn't get that");
+        }
     }
 }
 
-run(process.argv[2], process.argv.slice(3).join(" "));
+liribot.run(userCommand, dataInfo);
 
 
